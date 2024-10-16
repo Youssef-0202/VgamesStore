1a96ccf19dd0b238487f6a27642d625f2ca8e327
Youssef_AB <youssefaitbahssine23@gmail.com>
2024-10-16 09:49:44 +0100

    build color mode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,13 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/Dark Abstract Black Panther Gaming Logo (1).png";
 import React from "react";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize="80px" />
-      <Text>Nav bar</Text>
+      <ColorModeSwitch></ColorModeSwitch>
     </HStack>
   );
 };
