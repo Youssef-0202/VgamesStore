4a12ecdad6f4b325fec4a97387830fd159382488
Youssef_AB <youssefaitbahssine23@gmail.com>
2024-10-16 13:30:18 +0100

    displaying the genres picturs

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,16 @@ function App() {
         base: `"nav " "main"`,
         lg: `"nav nav" "aside main"`,
       }}
+      templateColumns={{
+        base: "1fr",
+        lg: "180px 1fr",
+      }}
     >
       <GridItem area="nav">
         <NavBar></NavBar>
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">
+        <GridItem area="aside" paddingX={"6px"}>
           <GenreList></GenreList>
         </GridItem>
       </Show>
